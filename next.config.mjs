fdcfe037db52cdcc6280a567fd88c09c434ec4aa
Stephen Parker <sparker888@gmail.com>
2024-06-18 16:06:32 -0400

    another seo change

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
-import withMarkdoc from '@markdoc/next.js'
-import withSearch from './src/markdoc/search.mjs'
+import withMarkdoc from '@markdoc/next.js';
+import withSearch from './src/markdoc/search.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-  experimental: {
-    metadataBase: new URL('https://gravital-wri-case-study.netlify.app/'),
-  },
-}
+  metadataBase: new URL('https://gravital-wri-case-study.netlify.app/'),
+};
 
 export default withSearch(
   withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
-)
+);
