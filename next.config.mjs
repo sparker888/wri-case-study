0a4e445e988304ff0114df04bc62136ef3ac265f
Stephen Parker <sparker888@gmail.com>
2024-07-30 13:37:37 -0400

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import withSearch from './src/markdoc/search.mjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-  metadataBase: new URL('https://gravital-wri-case-study.netlify.app/'),
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
