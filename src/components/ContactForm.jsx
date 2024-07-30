f44170c8371bc8926f198f8b996e0b18087e1aa6
Stephen Parker <sparker888@gmail.com>
2024-07-30 12:58:06 -0400

    updates to form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,15 +8,15 @@ import {
 
 export default function ContactForm() {
   return (
-    <div className="bg-gradient-to-br from-sky-900 to-amber-900 py-16">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="py-16 bg-gradient-to-br from-sky-900 to-amber-900">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           {/* Left column */}
           <div className="text-white">
             <img
               src="/images/gravital.png"
               alt="Gravital Digital"
-              className="mb-6 h-32 w-32"
+              className="w-32 h-32 mb-6"
             />
             <h2 className="mb-4 text-3xl font-bold">
               <span className="font-logo text-sky-500">Gravital</span>
@@ -30,15 +30,15 @@ export default function ContactForm() {
             </p>
             <div className="space-y-4">
               <div className="flex items-center">
-                <PhoneIcon className="mr-2 h-6 w-6 text-sky-400" />
+                <PhoneIcon className="w-6 h-6 mr-2 text-sky-400" />
                 <span>407-710-5497</span>
               </div>
               <div className="flex items-center">
-                <EnvelopeIcon className="mr-2 h-6 w-6 text-amber-400" />
+                <EnvelopeIcon className="w-6 h-6 mr-2 text-amber-400" />
                 <span>go@gravitaldigital.com</span>
               </div>
               <div className="flex items-center">
-                <CalendarIcon className="mr-2 h-6 w-6 text-amber-400" />
+                <CalendarIcon className="w-6 h-6 mr-2 text-amber-400" />
                 <a
                   href="https://calendly.com/book-sparker888/30min"
                   className="text-sky-400 hover:text-sky-300"
@@ -56,7 +56,7 @@ export default function ContactForm() {
               method="POST"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
-              className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+              className="grid grid-cols-1 mt-6 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
             >
               <input type="hidden" name="form-name" value="contact" />
               <p hidden>
@@ -67,7 +67,7 @@ export default function ContactForm() {
               <div>
                 <label
                   htmlFor="first-name"
-                  className="text-medium block pl-2 font-medium text-sky-300"
+                  className="block pl-2 font-medium text-medium text-sky-300"
                 >
                   First name
                 </label>
@@ -77,14 +77,14 @@ export default function ContactForm() {
                     name="first-name"
                     id="first-name"
                     autoComplete="given-name"
-                    className="block w-full rounded-md border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="last-name"
-                  className="text-medium block pl-2 font-medium text-sky-300"
+                  className="block pl-2 font-medium text-medium text-sky-300"
                 >
                   Last name
                 </label>
@@ -94,14 +94,14 @@ export default function ContactForm() {
                     name="last-name"
                     id="last-name"
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="email"
-                  className="text-medium block pl-2 font-medium text-sky-300"
+                  className="block pl-2 font-medium text-medium text-sky-300"
                 >
                   Email
                 </label>
@@ -111,7 +111,7 @@ export default function ContactForm() {
                     name="email"
                     type="email"
                     autoComplete="email"
-                    className="block w-full rounded-md border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                   />
                 </div>
               </div>
@@ -119,13 +119,13 @@ export default function ContactForm() {
                 <div className="flex justify-between">
                   <label
                     htmlFor="phone"
-                    className="text-medium block pl-2 font-medium text-sky-300"
+                    className="block pl-2 font-medium text-medium text-sky-300"
                   >
                     Phone
                   </label>
                   <span
                     id="phone-optional"
-                    className="text-medium pl-2 text-amber-200"
+                    className="pl-2 text-medium text-amber-200"
                   >
                     Optional
                   </span>
@@ -136,15 +136,15 @@ export default function ContactForm() {
                     name="phone"
                     id="phone"
                     autoComplete="tel"
-                    className="block w-full rounded-md border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                     aria-describedby="phone-optional"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
                 <label
-                  htmlFor="subject"
-                  className="text-medium block pl-2 font-medium text-sky-300"
+                  htmlFor="website"
+                  className="block pl-2 font-medium text-medium text-sky-300"
                 >
                   Website address
                 </label>
@@ -153,7 +153,7 @@ export default function ContactForm() {
                     type="text"
                     name="website"
                     id="website"
-                    className="block w-full rounded-md border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                   />
                 </div>
               </div>
@@ -161,13 +161,13 @@ export default function ContactForm() {
                 <div className="flex justify-between">
                   <label
                     htmlFor="message"
-                    className="text-medium block pl-2 font-medium text-sky-300"
+                    className="block pl-2 font-medium text-medium text-sky-300"
                   >
                     Message
                   </label>
                   <span
                     id="message-max"
-                    className="text-medium pl-2 text-amber-200"
+                    className="pl-2 text-medium text-amber-200"
                   >
                     Max. 500 characters
                   </span>
@@ -177,7 +177,7 @@ export default function ContactForm() {
                     id="message"
                     name="message"
                     rows={4}
-                    className="block w-full rounded-md border border-slate-400 bg-white px-4 py-3 text-slate-900 shadow-sm focus:border-amber-400 focus:ring-amber-400"
+                    className="block w-full px-4 py-3 bg-white border rounded-md shadow-sm border-slate-400 text-slate-900 focus:border-amber-400 focus:ring-amber-400"
                     aria-describedby="message-max"
                     defaultValue={''}
                   />
@@ -186,7 +186,7 @@ export default function ContactForm() {
               <div className="sm:col-span-2 sm:flex sm:justify-end">
                 <button
                   type="submit"
-                  className="inline-block rounded-full border border-transparent bg-sky-600 px-4 py-2 font-sans text-base font-medium text-amber-50 transition duration-500 hover:bg-sky-900 hover:text-amber-200 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2 sm:w-auto"
+                  className="inline-block px-4 py-2 font-sans text-base font-medium transition duration-500 border border-transparent rounded-full bg-sky-600 text-amber-50 hover:bg-sky-900 hover:text-amber-200 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2 sm:w-auto"
                 >
                   Send Message
                 </button>
