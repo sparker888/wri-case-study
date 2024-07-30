309231013ed5e00aad59c5603f196ea5a753c0b7
Stephen Parker <sparker888@gmail.com>
2024-07-30 13:26:17 -0400

    update form, config and add toml

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,19 @@ import withSearch from './src/markdoc/search.mjs';
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
   metadataBase: new URL('https://gravital-wri-case-study.netlify.app/'),
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    // Add custom webpack configuration here if needed
+    return config;
+  },
 };
 
 export default withSearch(
-  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
+  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig)
 );
