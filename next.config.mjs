23a499873b72d7836ea77cc513aecb3fd8d142f1
Stephen Parker <sparker888@gmail.com>
2024-07-30 16:18:09 -0400

    add for changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,23 @@
-import withMarkdoc from '@markdoc/next.js'
-import withSearch from './src/markdoc/search.mjs'
+import withMarkdoc from '@markdoc/next.js';
+import withSearch from './src/markdoc/search.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,  // Add this block for image optimization
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         fs: false,
-      }
+      };
     }
-    return config
+    return config;
   },
-}
+};
 
 export default withSearch(
-  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
-)
+  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig)
+);
